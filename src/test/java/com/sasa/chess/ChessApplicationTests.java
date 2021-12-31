d6bc2c84e11513a1ae08b4e5044a99161e19557a
kareemsasa3 <kareemsasa3@gmail.com>
2021-12-30 18:17:30 -0600

    initial project template

---
+++ src/test/java/com/sasa/chess/ChessApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sasa.chess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest
+class ChessApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertNull(null);
+	}
+
+}
